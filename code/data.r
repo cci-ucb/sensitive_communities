2abd539de6daac86c8be1dfd171120961a6625dc
Timothy Thomas <timothythomas@timothysmacbook.gateway.sonic.net>
2019-10-15 19:42:58 -0700

    update tmap

--- code/data.r
+++ code/data.r
@@ -15,8 +15,8 @@
 # ==========================================================================
 
 library(data.table)
+library(spdep)
 library(tidyverse)
-library(sf)
 library(tigris)
 options(tigris_use_cache = TRUE)
 library(tidycensus)
@@ -26,6 +26,10 @@ library(tmap)
 # Data
 # ==========================================================================
 
+	rm(list = ls())
+	options(scipen = 10) # avoid scientific notation
+
+
 #
 # State tract data
 # --------------------------------------------------------------------------
@@ -34,14 +38,12 @@ library(tmap)
 	source("~/git/sensitive_communities/code/vars.r")
 
 # Download CA tracts as ESRI shapefiles
-	caltracts <- 
-		tracts(state = "CA")
 
-	calpumas <- 
+	cp <- 
 		pumas(state = "CA")
 
 # Download CA tract data for select variables
-	caltractdata <-
+	ct_data <-
 		get_acs(
 			geography = "tract",
 			variables = dis_var,
@@ -52,7 +54,7 @@ library(tmap)
 			output = "wide") %>%
 		select(-ends_with("M")) 
 
-	calpumadata <- 
+	cp_data <- 
 		get_acs(
 			geography = "public use microdata area", 
 			variables = dis_var, 
@@ -61,9 +63,10 @@ library(tmap)
 			geometry = FALSE, 
 			cache_table = TRUE, 
 			output = "wide"
-			)
+			) %>%
+		select(-ends_with("M")) 
 
-	calcountydata <- 
+	cc_data <- 
 		get_acs(
 			geography = "county", 
 			variables = dis_var, 
@@ -72,38 +75,74 @@ library(tmap)
 			geometry = FALSE, 
 			cache_table = TRUE, 
 			output = "wide"
-			) 
-		
-caltracts@data <- 
-	left_join(caltracts@data, 
-			  caldata, 
+			) %>%
+		group_by(GEOID) %>% 
+		summarise(
+			   co_medinc = mhhincE, 
+			   co_li = co_medinc*.8, 
+			   co_vli = co_medinc*.5, 
+			   co_eli = co_medinc*.3, 
+			   co_rentprop = totrentE/tottenE, 
+			   co_rb30 = (rb_34.9E+rb_39.9E+rb_49.9E+rb_55E)/rb_totE, 
+			   co_rb50 = rb_55E/rb_totE, 
+			   co_medrent = medrentE)
+
+ct <- 
+	tracts(state = "CA")
+
+ct@data <- 
+	left_join(ct@data, 
+			  ct_data, 
 			  by = "GEOID")
+
+ct@data[is.na(ct@data)] <- 0
+
+# ct_data <- 
+# 	left_join(ct_data, 
+# 			  ct@data,
+# 			  by = c("GEOID" = "COUNTYFP")) %>% 
+# 	left_join(., 
+# 			  cc_data)
+	
+
+
+#
+# Create share of low income households: 50% ami > county median
+# --------------------------------------------------------------------------
+
+# County 
 
 
 #
 # Create Lag Variables
 # --------------------------------------------------------------------------
 
-	coords <- coordinates(caltracts)
-	IDs <- row.names(as(caltracts, "data.frame"))
-	caltracts_nb <- poly2nb(caltracts) # nb
-	lw_bin <- nb2listw(caltracts_nb, style = "B",zero.policy = TRUE)
+	coords <- coordinates(ct)
+	IDs <- row.names(as(ct, "data.frame"))
+	ct_nb <- poly2nb(ct) # nb
+	lw_bin <- nb2listw(ct_nb, style = "B", zero.policy = TRUE)
 	kern1 <- knn2nb(knearneigh(coords, k = 1), row.names=IDs)
 	dist <- unlist(nbdists(kern1, coords)); summary(dist)
 	max_1nn <- max(dist)
-	dist_nb <- dnearneigh(coords, d1=0, d2 = .75*max_1nn, row.names = IDs)
+	dist_nb <- dnearneigh(coords, d1=0, d2 = .1*max_1nn, row.names = IDs)
 	### listw object
 	set.ZeroPolicyOption(TRUE)
 	set.ZeroPolicyOption(TRUE)
-	dists <- nbdists(dist_nb, coordinates(caltracts))
+	dists <- nbdists(dist_nb, coordinates(ct))
 	idw <- lapply(dists, function(x) 1/(x^2))
 	lw_dist_idwW <- nb2listw(dist_nb, glist = idw, style = "W")
 
 ### lag values
-	caltracts$pED15.lag <- lag.listw(lw_dist_idwW,caltracts$pED15)
-	caltracts$pPOV00.lag <- lag.listw(lw_dist_idwW,caltracts$pPOV00)
-	caltracts$pPOV15.lag <- lag.listw(lw_dist_idwW,caltracts$pPOV15)
-	caltracts$pRB30Plus_15.lag <- lag.listw(lw_dist_idwW,caltracts$pRB30Plus_15)
-	caltracts$pRB50Plus_15.lag <- lag.listw(lw_dist_idwW,caltracts$pRB50Plus_15)
+	ct$medrent.lag <- lag.listw(lw_dist_idwW,ct$medrentE)
+	# ct$pPOV00.lag <- lag.listw(lw_dist_idwW,ct$pPOV00)
+	# ct$pPOV15.lag <- lag.listw(lw_dist_idwW,ct$pPOV15)
+	# ct$pRB30Plus_15.lag <- lag.listw(lw_dist_idwW,ct$pRB30Plus_15)
+	# ct$pRB50Plus_15.lag <- lag.listw(lw_dist_idwW,ct$pRB50Plus_15)
 
-	glimpse(wa_tracts@data)
+	# glimpse(wa_tracts@data)
+
+	tmap_mode("view")
+	tm_shape(ct) + 
+		tm_polygons(c("medrentE", "medrent.lag"), alphs = .5) + 
+		tm_facets(sync = TRUE, ncol = 2) + 
+		tm_view(set.view = c(lon = -122.2712, lat = 37.8044, zoom = 5))
