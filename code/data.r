441c4f9b34feeda2a058919f05eed55eb611be3e
Tim <timothyathomas@gmail.com>
2019-10-22 20:24:49 -0700

    edits

--- code/data.r
+++ code/data.r
@@ -35,11 +35,11 @@ library(tmap)
 # State tract data
 # --------------------------------------------------------------------------
 
-# Pull in variables
+# Pull in variables list
 	source("~/git/sensitive_communities/code/vars.r")
 
 # Download CA tract data for select variables
-	ct_data17 <-
+	tr_data17 <-
 		get_acs(
 			geography = "tract",
 			variables = dis_var,
@@ -51,7 +51,7 @@ library(tmap)
 			year = 2017) %>%
 		select(-ends_with("M"))
 
-	ct_data12 <-
+	tr_data12 <-
 		get_acs(
 			geography = "tract",
 			variables = dis_var,
@@ -62,53 +62,11 @@ library(tmap)
 			output = "wide",
 			year = 2012) %>%
 		select(-ends_with("M"))
-
-# Download CA block group data for select variables
-	cbg_data17 <-
-		get_acs(
-			geography = "block group",
-			variables = dis_var,
-			state = "CA",
-			county = NULL,
-			geometry = FALSE,
-			cache_table = TRUE,
-			output = "wide",
-			year = 2017) %>%
-		select(-ends_with("M"))
-
-### Block groups for 2008 to 2012 are not downloading
-	cbg_data12 <-
-		get_acs(
-			geography = "block group",
-			variables = dis_var,
-			state = "CA",
-			county = NULL,
-			geometry = FALSE,
-			cache_table = TRUE,
-			output = "wide",
-			year = 2012) %>%
-		select(-ends_with("M"))
-
-#
-# PUMA data - (ON HOLD)
-# --------------------------------------------------------------------------
-	# cp_data <-
-	# 	get_acs(
-	# 		geography = "public use microdata area",
-	# 		variables = dis_var,
-	# 		state = "CA",
-	# 		county = NULL,
-	# 		geometry = FALSE,
-	# 		cache_table = TRUE,
-	# 		output = "wide"
-	# 		) %>%
-	# 	select(-ends_with("M"))
-
 
 #
 # County data
 # --------------------------------------------------------------------------
-	cc_data <-
+	county_acsdata <- function(year)
 		get_acs(
 			geography = "county",
 			variables = dis_var,
@@ -116,7 +74,8 @@ library(tmap)
 			county = NULL,
 			geometry = FALSE,
 			cache_table = TRUE,
-			output = "wide"
+			output = "wide",
+			year = year
 			) %>%
 		group_by(GEOID) %>%
 		summarise(
@@ -127,85 +86,172 @@ library(tmap)
 			   co_rentprop = totrentE/tottenE,
 			   co_rb30 = (rb_34.9E+rb_39.9E+rb_49.9E+rb_55E)/rb_totE,
 			   co_rb50 = rb_55E/rb_totE,
-			   co_medrent = medrentE)
+			   co_medrent = medrentE,
+			   co_toted = totedE,
+			   co_bachplus = sum(bachE, masE, proE, docE),
+			   co_meded = median(co_bachplus/co_toted))
+
+	co_data <-
+		left_join(county_acsdata(2012), county_acsdata(2017), by = "GEOID")
 
 #
-# Download CA tracts as ESRI shapefiles - no data (will join below)
+# Download CA tracts as ESRI shapefiles with no data (will join below)
 # --------------------------------------------------------------------------
 
 # Download shapefile
 	ct <-
-		tracts(state = "CA")	
+		tracts(state = "CA")
 
-	cbg <-
-		block_groups(state = "CA")
 
 # create county join ID
 	ct@data <-
 		ct@data %>%
 		mutate(county = paste0(STATEFP, COUNTYFP))
 
-# create county join ID
-	cbg@data <-
-		cbg@data %>%
-		mutate(county = paste0(STATEFP, COUNTYFP))
-
 # Create new Cal object for posterity of OG ct object
 	cal_tracts <- ct
-	cal_bg <- cbg
 
 # Join County and ACS tract data to cal_tracts shapefile
 	cal_tracts@data <-
 		left_join(cal_tracts@data,
-				  cc_data,
+				  co_data,
 				  by = c("county" = "GEOID")) %>%
 		left_join(.,
-				  ct_data12,
+				  tr_data12,
 				  by = "GEOID") %>%
 		left_join(.,
-				  ct_data17,
+				  tr_data17,
 				  by = "GEOID")
 
+# ==========================================================================
+# Create variables
+# ==========================================================================
 
 #
-# Create household income breaks (how many households fall within certain
+# County Share of ELI households -
+#	Create household income breaks (how many households fall within certain
 # 	thresholds)
 # --------------------------------------------------------------------------
 
-newnames <-
-	c('HHInc_10E.x' = 10000,'HHInc_15E.x' = 14999,'HHInc_20E.x' = 19999,'HHInc_25E.x' = 24999,'HHInc_30E.x' = 29999,'HHInc_35E.x' = 34999,'HHInc_40E.x' = 39999,'HHInc_45E.x' = 44999,'HHInc_50E.x' = 49999,'HHInc_60E.x' = 59999,'HHInc_75E.x' = 74999,'HHInc_100E.x' = 99999,'HHInc_125E.x' = 124999,'HHInc_150E.x' = 149999,'HHInc_200E.x' = 199999,'HHInc_250E.x' = 200000)
+newnames12 <-
+	c('HHInc_10E.x' = 9999,'HHInc_15E.x' = 14999,'HHInc_20E.x' = 19999,'HHInc_25E.x' = 24999,'HHInc_30E.x' = 29999,'HHInc_35E.x' = 34999,'HHInc_40E.x' = 39999,'HHInc_45E.x' = 44999,'HHInc_50E.x' = 49999,'HHInc_60E.x' = 59999,'HHInc_75E.x' = 74999,'HHInc_100E.x' = 99999,'HHInc_125E.x' = 124999,'HHInc_150E.x' = 149999,'HHInc_200E.x' = 199999,'HHInc_250E.x' = 199999)
 
-	hhinc12 <-
+	trli12 <-
 		cal_tracts@data %>%
-		select(GEOID, county, co_medinc, HHInc_TotalE.x:HHInc_250E.x) %>%
+		select(GEOID, COUNTYFP, co_medinc.x, HHInc_TotalE.x:HHInc_250E.x) %>%
 		group_by(GEOID) %>%
-		mutate(LI_val = co_medinc*.8,
-			   VLI_val = co_medinc*.5,
-			   ELI_val = co_medinc*.3) %>%
+		mutate(LI_val = co_medinc.x*.8,
+			   VLI_val = co_medinc.x*.5,
+			   ELI_val = co_medinc.x*.3) %>%
 		gather(medinc_cat, medinc_cat_count, HHInc_10E.x:HHInc_250E.x) %>%
-		mutate_at(vars(medinc_cat), ~newnames) %>%
-		mutate(LI = if_else(ELI_val >= medinc_cat, 1, 0),
-			   VLI = if_else(VLI_val >= medinc_cat, 1, 0),
-			   ELI = if_else(ELI_val >= medinc_cat, 1, 0)) %>%
+		mutate_at(vars(medinc_cat), ~newnames12) %>%
+		mutate(bottom_inccat = if_else(medinc_cat <= 9999, medinc_cat - 9999,
+							   if_else(medinc_cat <= 49999 &
+							   		   medinc_cat >= 10000, medinc_cat - 4999,
+								if_else(medinc_cat >= 50000 &
+										medinc_cat <= 99999, medinc_cat -24999,
+								if_else(medinc_cat >= 100000 &
+										medinc_cat <= 199999, medinc_cat - 49999,
+								NA_real_)))),
+			   top_inccat = medinc_cat) %>%
+		mutate(LI = if_else(LI_val >= top_inccat, 1,
+					if_else(LI_val <= top_inccat &
+			   					LI_val >= bottom_inccat,
+			   					(LI_val - bottom_inccat)/(top_inccat - bottom_inccat), 0)),
+			   VLI = if_else(VLI_val >= top_inccat, 1,
+					 if_else(VLI_val <= top_inccat &
+			   					VLI_val >= bottom_inccat,
+			   					(VLI_val - bottom_inccat)/(top_inccat - bottom_inccat), 0)),
+			   ELI = if_else(ELI_val >= top_inccat, 1,
+					 if_else(ELI_val <= top_inccat &
+			   					ELI_val >= bottom_inccat,
+			   					(ELI_val - bottom_inccat)/(top_inccat - bottom_inccat), 0)),
+			   tr_totalinc12 = sum(medinc_cat_count),
+			   tr_LI_count12 = sum(LI*medinc_cat_count),
+			   tr_VLI_count12 = sum(VLI*medinc_cat_count),
+			   tr_ELI_count12 = sum(ELI*medinc_cat_count),
+			   tr_LI_prop12 = tr_LI_count12/tr_totalinc12,
+			   tr_VLI_prop12 = tr_VLI_count12/tr_totalinc12,
+			   tr_ELI_prop12 = tr_ELI_count12/tr_totalinc12) %>%
+		select(GEOID, COUNTYFP, tr_totalinc12:tr_ELI_prop12) %>%
+		distinct() %>%
+		ungroup()
 
+newnames17 <-
+	c('HHInc_10E.y' = 9999,'HHInc_15E.y' = 14999,'HHInc_20E.y' = 19999,'HHInc_25E.y' = 24999,'HHInc_30E.y' = 29999,'HHInc_35E.y' = 34999,'HHInc_40E.y' = 39999,'HHInc_45E.y' = 44999,'HHInc_50E.y' = 49999,'HHInc_60E.y' = 59999,'HHInc_75E.y' = 74999,'HHInc_100E.y' = 99999,'HHInc_125E.y' = 124999,'HHInc_150E.y' = 149999,'HHInc_200E.y' = 199999,'HHInc_250E.y' = 199999)
 
-	setnames(hhinc12, old = )
+	trli17 <-
+		cal_tracts@data %>%
+		select(GEOID, COUNTYFP, co_medinc.y, HHInc_TotalE.y:HHInc_250E.y) %>%
+		group_by(GEOID) %>%
+		mutate(LI_val = co_medinc.y*.8,
+			   VLI_val = co_medinc.y*.5,
+			   ELI_val = co_medinc.y*.3) %>%
+		gather(medinc_cat, medinc_cat_count, HHInc_10E.y:HHInc_250E.y) %>%
+		mutate_at(vars(medinc_cat), ~newnames17) %>%
+		mutate(bottom_inccat = if_else(medinc_cat <= 9999, medinc_cat - 9999,
+							   if_else(medinc_cat <= 49999 &
+							   		   medinc_cat >= 10000, medinc_cat - 4999,
+								if_else(medinc_cat >= 50000 &
+										medinc_cat <= 99999, medinc_cat -24999,
+								if_else(medinc_cat >= 100000 &
+										medinc_cat <= 199999, medinc_cat - 49999,
+								NA_real_)))),
+			   top_inccat = medinc_cat) %>%
+		mutate(LI = if_else(LI_val >= top_inccat, 1,
+					if_else(LI_val <= top_inccat &
+			   					LI_val >= bottom_inccat,
+			   					(LI_val - bottom_inccat)/(top_inccat - bottom_inccat), 0)),
+			   VLI = if_else(VLI_val >= top_inccat, 1,
+					 if_else(VLI_val <= top_inccat &
+			   					VLI_val >= bottom_inccat,
+			   					(VLI_val - bottom_inccat)/(top_inccat - bottom_inccat), 0)),
+			   ELI = if_else(ELI_val >= top_inccat, 1,
+					 if_else(ELI_val <= top_inccat &
+			   					ELI_val >= bottom_inccat,
+			   					(ELI_val - bottom_inccat)/(top_inccat - bottom_inccat), 0)),
+			   tr_totalinc17 = sum(medinc_cat_count),
+			   tr_LI_count17 = sum(LI*medinc_cat_count),
+			   tr_VLI_count17 = sum(VLI*medinc_cat_count),
+			   tr_ELI_count17 = sum(ELI*medinc_cat_count),
+			   tr_LI_prop17 = tr_LI_count17/tr_totalinc17,
+			   tr_VLI_prop17 = tr_VLI_count17/tr_totalinc17,
+			   tr_ELI_prop17 = tr_ELI_count17/tr_totalinc17) %>%
+		select(GEOID, COUNTYFP, tr_totalinc17:tr_ELI_prop17) %>%
+		distinct() %>%
+		ungroup()
 
+lidata <-
+	left_join(trli12, trli17, by = c("GEOID", "COUNTYFP")) %>%
+	group_by(COUNTYFP) %>%
+	mutate(co_totalinc12 = sum(tr_totalinc12),
+		   co_LI_count12 = sum(tr_LI_count12),
+		   co_VLI_count12 = sum(tr_VLI_count12),
+		   co_ELI_count12 = sum(tr_ELI_count12),
+		   co_LI_prop12 = co_LI_count12/co_totalinc12,
+		   co_VLI_prop12 = co_VLI_count12/co_totalinc12,
+		   co_ELI_prop12 = co_ELI_count12/co_totalinc12,
+		   co_LI_propmed12 = median(tr_LI_prop12, na.rm = TRUE),
+		   co_VLI_propmed12 = median(tr_VLI_prop12, na.rm = TRUE),
+		   co_ELI_propmed12 = median(tr_ELI_prop12, na.rm = TRUE),
+		   co_totalinc17 = sum(tr_totalinc17),
+		   co_LI_count17 = sum(tr_LI_count17),
+		   co_VLI_count17 = sum(tr_VLI_count17),
+		   co_ELI_count17 = sum(tr_ELI_count17),
+		   co_LI_prop17 = co_LI_count17/co_totalinc17,
+		   co_VLI_prop17 = co_VLI_count17/co_totalinc17,
+		   co_ELI_prop17 = co_ELI_count17/co_totalinc17,
+		   co_LI_propmed17 = median(tr_LI_prop17, na.rm = TRUE),
+		   co_VLI_propmed17 = median(tr_VLI_prop17, na.rm = TRUE),
+		   co_ELI_propmed17 = median(tr_ELI_prop17, na.rm = TRUE))
 
-		%>%
-		rename_at(vars)
-		rename_at(vars(HHInc_10E.x:HHInc_250E.x), list(renames))
+glimpse(lidata)
 
-### LEFT OFF ###
-# Note, EL Measures
-# Rent gap
-# Change in rent
-# Educational attainment
-# Bachelors
-# Income segregation by race
-# Rent burden (high local, low nearby)
-###
+#
+# Change
+# --------------------------------------------------------------------------
 
+	left_join(cal_tracts@data, lidata, by = c("GEOID", "COUNTYFP")) %>%
 
 
 #
@@ -378,12 +424,12 @@ wt@data <-
 
 wt@data[is.na(wt@data)] <- 0
 
-# ct_data <-
-# 	left_join(ct_data,
+# tr_data <-
+# 	left_join(tr_data,
 # 			  ct@data,
 # 			  by = c("GEOID" = "COUNTYFP")) %>%
 # 	left_join(.,
-# 			  cc_data)
+# 			  co_data)
 
 
 
@@ -460,3 +506,57 @@ king@data <-
 # --------------------------------------------------------------------------
 
 	test <- pumas(state = "CA")
+
+#
+# block group data
+# --------------------------------------------------------------------------
+
+	# cbg <-
+	# 	block_groups(state = "CA")
+
+# create county join ID
+	# cbg@data <-
+	# 	cbg@data %>%
+	# 	mutate(county = paste0(STATEFP, COUNTYFP))
+	# cal_bg <- cbg
+
+# Download CA block group data for select variables
+	# cbg_data17 <-
+	# 	get_acs(
+	# 		geography = "block group",
+	# 		variables = dis_var,
+	# 		state = "CA",
+	# 		county = NULL,
+	# 		geometry = FALSE,
+	# 		cache_table = TRUE,
+	# 		output = "wide",
+	# 		year = 2017) %>%
+	# 	select(-ends_with("M"))
+
+### Block groups for 2008 to 2012 are not downloading
+	# cbg_data12 <-
+	# 	get_acs(
+	# 		geography = "block group",
+	# 		variables = dis_var,
+	# 		state = "CA",
+	# 		county = NULL,
+	# 		geometry = FALSE,
+	# 		cache_table = TRUE,
+	# 		output = "wide",
+	# 		year = 2012) %>%
+	# 	select(-ends_with("M"))
+
+#
+# PUMA data - (ON HOLD)
+# --------------------------------------------------------------------------
+	# cp_data <-
+	# 	get_acs(
+	# 		geography = "public use microdata area",
+	# 		variables = dis_var,
+	# 		state = "CA",
+	# 		county = NULL,
+	# 		geometry = FALSE,
+	# 		cache_table = TRUE,
+	# 		output = "wide"
+	# 		) %>%
+	# 	select(-ends_with("M"))
